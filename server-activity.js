c7219b5f74fe9ff1ac609be51f96137465da96e1
Jason Lin <jlin0402@gmail.com>
2017-12-09 11:25:46 -0800

    added seperate var for each portto avoid overlapping declaration

--- server-activity.js
+++ server-activity.js
@@ -1,16 +1,30 @@
 var http = require ("http");
 //create http that listen on port 7000
-var PORT = 7000;
+var GOOD_PORT = 7000;
 
 //port 7000 always tells user something good about themselves
-function handleRequest(req, res) {
-    response.end("You look good");
+function handleGoodReq(req, res) {
+    response.end("You've made a server: " + req.url);
 };
+
+var goodServer = http.createServer(handleGoodReq);
+
+goodServer.listen(GOOD_PORT, function(){
+    console.log("Listening on: http://localhost:" + GOOD_PORT);
+    console.log("You look good");
+});
 
 //create port that listen on port 7500
-var PORT = 7500;
+var BAD_PORT = 7500;
 
 //port 7500 always tells user something bad about themselves
-function handleRequest(req, res) {
-    response.end("You look like shiet");
+function handleBadReq(req, res) {
+    response.end("Listening on: http://localhost:" + BAD_PORT);
 };
+
+var badServer = http.createServer(handleBadReq);
+
+badServer.listen(BAD_PORT, function(){
+    console.log("Listening on: http://localhost:" + BAD_PORT);
+    console.log("You look like crap, de heck's a matter with you?");
+});
