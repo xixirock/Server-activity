09e482b826aa5d33f864f51e5cef00983670ee11
Jason Lin <jlin0402@gmail.com>
2017-12-09 11:16:00 -0800

    added port response

---
+++ server-activity.js
@@ -0,0 +1,16 @@
+var http = require ("http");
+//create http that listen on port 7000
+var PORT = 7000;
+
+//port 7000 always tells user something good about themselves
+function handleRequest(req, res) {
+    response.end("You look good");
+};
+
+//create port that listen on port 7500
+var PORT = 7500;
+
+//port 7500 always tells user something bad about themselves
+function handleRequest(req, res) {
+    response.end("You look like shiet");
+};
